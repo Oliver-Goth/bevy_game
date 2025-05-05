6153aac06637857c2c93dce471f54c7f68bbc61d
Oliver-Goth <Olivergoth00@gmail.com>
2025-05-05 15:28:24 +0200

    player movement and animation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+mod animation;
 mod map;
 mod movement;
 mod player;
@@ -8,6 +9,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (map::spawn_map, player::spawn_player))
-        .add_systems(Update, movement::player_movement)
+        .add_systems(Update, (movement::player_movement, animation::animate_sprite))
         .run();
 }
